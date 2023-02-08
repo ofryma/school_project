df1fb7b04a36aca91dfc4d3e9d5f5bb07de55cfc
Ofry Makdasy <101065365+ofryma@users.noreply.github.com>
2023-02-08 20:49:03 +0000

    itamar calculation

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ from fastapi import File, UploadFile
 from fastapi.responses import FileResponse , HTMLResponse
 import uvicorn
 from pydantic import BaseModel
-
+import scipy
+from math import pow
+import openpyxl
 import io
 
 from analyze import *
@@ -22,6 +24,70 @@ app = FastAPI(
     )
 
 
+
+
+
+def find_value(index : str , tag_len: int , data : str , close_tag : str = "$"):
+
+    i = index + tag_len
+    value = ""
+    while not data[i] == close_tag:
+        value += data[i]
+        i+=1
+        if i >= len(data):
+            break
+
+    return value
+
+
+def kappa(ref_dens : float , defu : float , cp : float):
+    return ref_dens * defu * cp
+
+
+
+def kappa_calc(data):
+
+    data_dict = {
+        'REF_DENSITY' : None,
+        'SAMPLE_NAME' : None,
+        'Shot_number' : [],
+        'Temperature_lfa' : [],
+        'Diffusivity' : [],
+        'Std_dev' : [],
+        'Cp_calc' : [],
+        'Kappa' : []
+    }
+    
+    try:
+        data_dict["REF_DENSITY"] = float(find_value(data.index("#Ref_density /(g/cm^3),")  , len("#Ref_density /(g/cm^3),"), data , close_tag = "\\"))
+        data_dict["SAMPLE_NAME"] = find_value(data.index("#Sample,")  , len("#Sample,"), data , close_tag = "\\")
+        data = find_value(data.index("#Cp-calc./(J/g/K)\\r\\n") , len("#Cp-calc./(J/g/K)\\r\\n") , data)
+    except:
+        pass
+    
+    for row in data.split("\\r\\n"):
+        
+        try:
+            row = row.split(",")
+            data_dict["Shot_number"].append(row[0])
+            data_dict["Temperature_lfa"].append(float(row[1]) + 273.0)    
+            data_dict["Diffusivity"].append(float(row[2]))    
+            data_dict["Std_dev"].append(float(row[3]))    
+            data_dict["Cp_calc"].append(float(row[4])) 
+            data_dict["Kappa"].append(kappa(
+                ref_dens = data_dict["REF_DENSITY"],
+                defu = float(row[2]),
+                cp = float(row[4])
+            )) 
+        except:
+            pass
+    
+    return data_dict
+
+
+
+
+
 @app.get("/download-data" , tags=["Data files"])
 async def download_file():
     cell_data_file_path = "cell-data.csv"
@@ -31,6 +97,72 @@ async def download_file():
     
     return FileResponse(cell_data_file_path , media_type='application/octet-stream',filename=cell_data_file_path)
     
+
+@app.post("/itamar/raw-data-file")
+async def zt_file_analyzer(
+    lfa_file: UploadFile = File(...),
+    lsr_file: UploadFile = File(...),
+):
+
+
+
+    data_dict = kappa_calc(str(lfa_file.file.read()))
+
+
+    with open(lsr_file.filename, "wb") as binary_file:
+    
+        # Write bytes to file
+        binary_file.write(lsr_file.file._file.read())
+    
+    df = pd.read_excel(lsr_file.filename)
+
+    data_dict["Temperature_lsr"] = [val + 273.0 for val in df["Temperature(°C)"]]
+    data_dict["Seebeck_cof"] = [val for val in df["Seebeck coefficient(µV/K)"]]
+    data_dict["Resistivity"] = [val * 10**5 for val in df["Resistivity(??m)"]]
+    
+
+
+    kappa_func = scipy.interpolate.PchipInterpolator(data_dict["Temperature_lfa"], data_dict["Kappa"] , extrapolate=True)
+    
+    data_dict["ZT"] = []
+    data_dict["Power_Factor"] = []
+    data_dict["Kappa_Interp"] = []
+    for s , t , r in zip(data_dict["Seebeck_cof"] , data_dict["Temperature_lsr"] , data_dict["Resistivity"]):
+        data_dict["Kappa_Interp"].append(kappa_func.__call__(t))
+        data_dict["ZT"].append(((s**2)*t)/(kappa_func.__call__(t)*r*pow(10,7)))
+        data_dict["Power_Factor"].append(pow(s , 2) / (r*pow(10,7)))
+
+    del data_dict["REF_DENSITY"]
+    del data_dict["SAMPLE_NAME"]
+
+
+    max_len = 0
+    for key in data_dict.keys():
+        try:
+            if len(data_dict[key]) > max_len:
+                max_len = len(data_dict[key])
+        except:
+            pass
+
+
+
+    for key in data_dict.keys():
+        for i in range(max_len):
+            try:
+               if data_dict[key][i] == None:
+                pass
+            except:
+                data_dict[key].append("")
+    
+    if os.path.exists(lfa_file.filename):
+        os.remove(lfa_file.filename)
+    if os.path.exists(lsr_file.filename):
+        os.remove(lsr_file.filename)
+
+    df = pd.DataFrame.from_dict(data_dict)
+    df.to_csv("data.csv")
+
+    return FileResponse("data.csv" , media_type='application/octet-stream',filename=lfa_file.filename)
 
 
 
